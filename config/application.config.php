58f62838e90bdf3a3a13ff426d34b8260e241224
girish-ankit <girish.ankit@gmail.com>
2014-11-14 17:33:59 +0530

    added my Users module information to application config file

--- config/application.config.php
+++ config/application.config.php
@@ -8,7 +8,8 @@
 return array(
     'modules' => array(
         'Application',
-        'AnkitKumar'
+        'AnkitKumar',
+        'Users'
     ),
     'module_listener_options' => array(
         'module_paths' => array(
